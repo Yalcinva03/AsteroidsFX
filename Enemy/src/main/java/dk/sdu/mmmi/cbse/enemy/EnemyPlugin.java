2a0888bf45612bbe82ed5598310572ab8a133309
Yalcin Acili <yalcinacili@gmail.com>
2024-03-04 13:08:01 +0100

    enemy module added

--- Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyPlugin.java
+++ Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyPlugin.java
@@ -1,4 +1,18 @@
 package dk.sdu.mmmi.cbse.enemy;
 
-public class EnemyPlugin {
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class EnemyPlugin implements IGamePluginService {
+    @Override
+    public void start(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+
+    }
 }
